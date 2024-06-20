7abd6f47e06ae28cb8b814caa2f6ea6804dec9eb
dilipdevops5511 <dilipdevopscourses@gmail.com>
2024-06-20 13:57:52 +0530

    first commit

--- modules/rds/variables.tf
+++ modules/rds/variables.tf
@@ -1,8 +1,19 @@
-variable "sg-name" {}
-variable "private-subnet-name1" {}
-variable "private-subnet-name2" {}
-variable "db-sg-name" {}
-variable "rds-username" {}
-variable "rds-pwd" {}
-variable "db-name" {}
-variable "rds-name" {}
+variable "private_subnet_name1" {
+  description = "Name of the first private subnet"
+  type        = string
+}
+
+variable "private_subnet_name2" {
+  description = "Name of the second private subnet"
+  type        = string
+}
+
+variable "db_sg_name" {
+  description = "Name of the database security group"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the subnets and security group are located"
+  type        = string
+}
